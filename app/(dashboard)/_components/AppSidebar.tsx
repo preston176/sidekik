340f97617cc471dc2cced693cc752d01859e03ad
preston176 <prestonnyamweya@gmail.com>
2025-09-29 10:10:23 +0300

    feat: enhance AppSidebar with theme toggle and logo integration

--- app/(dashboard)/_components/AppSidebar.tsx
+++ app/(dashboard)/_components/AppSidebar.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
 import {
     Sidebar,
     SidebarContent,
@@ -5,11 +8,34 @@ import {
     SidebarGroup,
     SidebarHeader,
 } from "@/components/ui/sidebar"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import Image from "next/image"
 
 export function AppSidebar() {
+    const { theme, setTheme } = useTheme()
     return (
         <Sidebar>
-            <SidebarHeader />
+            <SidebarHeader>
+                <div className="p-3">
+                    <div className="flex items-center gap-3">
+                        <Image src={"/logo.png"} alt="Sidekik Logo" width={60} height={60} className="w-[40px] h-[40px]" />
+                        <h2 className="font-bold text-xl">Side<span>kik</span></h2>
+                    </div>
+                    <div>
+                        <Button
+                            onClick={() =>
+                                setTheme((current) => (current === "dark" ? "light" : "dark"))
+                            }
+                            variant={"ghost"}
+                        >
+                            {
+                                theme === "dark" ? <Sun /> : <Moon />
+                            }
+                        </Button>
+                    </div>
+                </div>
+            </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup />
                 <SidebarGroup />
