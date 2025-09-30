7dc47f59e2bb4716c31d4b27d6b5163795062aec
preston176 <prestonnyamweya@gmail.com>
2025-09-30 09:25:27 +0300

    feat: enable tour button in AppHeader for user guidance

--- app/(dashboard)/_components/AppHeader.tsx
+++ app/(dashboard)/_components/AppHeader.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@/components/ui/button'
 import { SidebarTrigger } from '@/components/ui/sidebar'
+import { HelpCircle } from 'lucide-react'
 import React from 'react'
 
 function AppHeader() {
     return (
         <div className='p-3 shadow flex justify-between items-center'>
             <SidebarTrigger />
-            {/* <Button>Take Tour</Button> */}
+            <Button><HelpCircle />Take Tour</Button>
         </div>
     )
 }
